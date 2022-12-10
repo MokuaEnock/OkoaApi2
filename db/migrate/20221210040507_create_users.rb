ca39693e2a758b8d84d74e6390c8d9447e4c4f84
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-10 07:14:28 +0300

    generate role resource

--- db/migrate/20221210040507_create_users.rb
+++ db/migrate/20221210040507_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.belongs_to :role, foreign_key: true
       t.string :username
       t.string :first_name
       t.string :last_name
