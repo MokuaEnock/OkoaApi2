9ff09f0afbafb47375ae21035063a0c132c1b23e
Enock Mokua <enock.mokua@student.moringaschool.com>
2022-12-14 04:47:51 +0300

    create user actions

--- app/controllers/concerns/jwt_token.rb
+++ app/controllers/concerns/jwt_token.rb
@@ -13,24 +13,5 @@ module JwtToken
     decoded = JWT.decode(token, SECRET_KEY)
     HashWithIndifferentAcess.new decoded
   end
-  
+
 end
-
-# module jwtToken
-#   extend ActiveSupport::Concern
-#   SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
-
-#   def jwt_encode[payload, exp: 7.days.from_now]
-#     payload[:exp] = exp.
-#     JWT.encode(payload, SECRET_KEY)
-#   end
-
-#   def jwt_decode(token)
-#     decoded = JWT.decode(token, SECRET_KEY)
-#     HashWithIndifferentAcess.new decoded
-#   end
-# end
-
-# module jwtToken
-
-# end
