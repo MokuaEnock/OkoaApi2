31f61dabd5355a681d9de58bb557dffa2af5a3eb
Enock Mokua <enock.mokua@student.moringaschool.com>
2022-12-14 04:14:02 +0300

    remove first name and last name

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_041228) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "first_name"
-    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
